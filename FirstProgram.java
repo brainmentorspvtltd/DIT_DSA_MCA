382d22237f7c4323487504d224fc403a67e27248
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-02-07 15:54:51 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,29 @@
+// This is called Single line comment
+/*
+ * This is Multi line comment
+ * Java is OOPS , so every program in java start with class.
+ */
+// class <ClassName>
+// what is class, class is a keyword (Reserve word), we declare a class
+// class name can be same or different if class is not public.
+// if class is public so it names must be match with a file.
+// class name should be in PascalCase e.g PrintWriter e.g AmitSrivastava
+// to compile java program e.g javac <FileName>.java
+// after compile we get a Byte Code (.class file), A Byte code file name is same as class name
+// to run java code e.g java <ByteCodeFileName/ClassName>
+/*
+ * java command trigger JRE, JRE Call JVM --> JVM Looking for main to execute
+ */
+class FirstClass{
+    // JVM Looking for main
+    // main - it is like a main gate ( main is a first function which start the code execution)
+    // void - it is a keyword , and it means main return nothing.
+    // static - static things will be loaded when class is loaded.
+    // when u run a program first it load in memory, 
+    //during the load time (The class load time) , static things will be loaded
+    // public is also a keyword , and it means public things access outside the folder/ package
+    // String args[] - it a command line argument
+    public static void main(String args[]){
+
+    }
+}
